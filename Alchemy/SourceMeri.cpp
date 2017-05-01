98f3de6b5a0a135dfa302be65b91743cda2c730b
Maria López <marialopezvivas@enti.cat>
2017-05-01 19:17:58 +0200

    Añadiendo el map

--- Alchemy/SourceMeri.cpp
+++ Alchemy/SourceMeri.cpp
@@ -2,21 +2,54 @@
 #include <ostream>
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 #include <map>
 #include <vector>
 #include <string>
+#include <utility>
+
+
+//para que no de error con el hash
+struct pair_hash {
+	template <class T1, class T2>
+	std::size_t operator () (const std::pair<T1, T2> &p) const {
+		auto h1 = std::hash<T1>{}(p.first);
+		auto h2 = std::hash<T2>{}(p.second);
+		return h1 ^ h2;
+	}
+};
+//creamos el pair
+using key = std::pair <std::string, std::string>;
+
 
 void Combinaciones()
 {
-	//leer y guardar el fichero en un unordered_map
+	//abrir archivo
 	std::ifstream fentrada;
 	fentrada.open("elements.dat");
+	//leer archivo y guardarlo
 	std::string linea;
+	//creamos la variable que usaremos de elemento
+	std::string element;
+	//creamos el map
+	std::unordered_map <key, std::string, pair_hash> combinaciones_map;
+	std::pair<std::string, std::string> key;
+	int contador = 1;
 	while (getline(fentrada, linea))
 	{
-		std::cout << linea << std::endl;
+		
+
+		combinaciones_map[key] = element;
+		//std::cout << linea << std::endl;
+		std::cout << element << "=" << key.first << "+" << key.second << std::endl;
+		contador++;
 	}
+		
+	//cerrar archivo
+	fentrada.close();
 }
+	
+
 
 void main()
 {
