2bde6c2b15a4ef084c867c71f68f82ad87d67c94
Maria López <marialopezvivas@enti.cat>
2017-05-02 01:04:41 +0200

    Implementadas las funciones (falta arreglar alguna)

--- Alchemy/SourceMeri.cpp
+++ Alchemy/SourceMeri.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
-#include <map>
 #include <vector>
 #include <string>
 #include <utility>
+#include <algorithm>
+#include <Windows.h>
 
 
-//para que no de error con el hash
+//Para que no de error con el hash
 struct pair_hash {
 	template <class T1, class T2>
 	std::size_t operator () (const std::pair<T1, T2> &p) const {
@@ -18,10 +19,13 @@ struct pair_hash {
 		return h1 ^ h2;
 	}
 };
-//creamos el pair
+//Creamos el pair
 using key = std::pair <std::string, std::string>;
 
+//Creamos el unordered_map
+std::unordered_map <key, std::string, pair_hash> combinaciones_map;
 
+//Leer y guardar archivo
 void Combinaciones()
 {
 	//abrir archivo
@@ -31,8 +35,7 @@ void Combinaciones()
 	std::string linea;
 	//creamos la variable que usaremos de elemento
 	std::string element;
-	//creamos el map
-	std::unordered_map <key, std::string, pair_hash> combinaciones_map;
+	//creamos variables necesarias
 	std::pair<std::string, std::string> key;
 	int espacio, mas, fin, longitud, inicio;
 	inicio = 0;
@@ -54,16 +57,83 @@ void Combinaciones()
 		inicio = 0;
 
 		combinaciones_map[key] = element;
-		std::cout << element << "=" << key.first << "+" << key.second << std::endl;	
+		//std::cout << element << "=" << key.first << "+" << key.second << std::endl;	
 	}
-		
 	//cerrar archivo
 	fentrada.close();
 }
-	
+std::vector <std::string> basics({"Air", "Earth", "Fire", "Water"});
+std::vector <std::string> elements({"Air", "Earth", "Fire", "Water"});
 
+//funcion para imprimir los nuevos elementos
+void Print()
+{
+	for (int i = 0; i < elements.size(); i++)
+	{
+		std::cout << i + 1 << " " << elements[i] << std::endl;
+	}
+}
+
+//funcion add
+void Add(int pos)
+{
+	elements.insert(elements.end(), elements[pos - 1]);
+}
+
+//funcion add basics
+void Addbasics()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		elements.insert(elements.end(), basics[i]);
+	}
+}
+
+//funcion delete
+void Delete(int pos)
+{
+	elements.erase(elements.begin() + pos - 1);
+}
+
+//funcion info
+void Info(int pos)
+{
+	std::string url;
+	url = "https://en.wikipedia.org/wiki/" + elements[pos - 1];
+	ShellExecuteA(nullptr, "open", url.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+}
+
+//funcion sort
+void Sort()
+{
+	std::sort(elements.begin(), elements.end());
+}
+
+//funcion clean
+void Clean()
+{
+	for (int i = 0; i < elements.size(); i++)
+	{
+		for (int j = i + 1; j < elements.size() - 2; j++)
+		{
+			if (elements[i] == elements [j])
+			{
+				Delete(j);
+			}
+		}
+	}
+}
 
 void main()
 {
+	std::string input;
 	Combinaciones();
+	//Addbasics();
+//	Sort();
+	//Print();
+	//system ("cls");
+	//Clean();
+	Print();
+	Info(3);
+	
 }
