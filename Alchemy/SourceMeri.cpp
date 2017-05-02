fb4bb1da54a45c5a9de1c3a40dcfd6c19c48b666
Maria López <marialopezvivas@enti.cat>
2017-05-02 23:22:57 +0200

    push arreglando cosas

--- Alchemy/SourceMeri.cpp
+++ Alchemy/SourceMeri.cpp
@@ -25,6 +25,9 @@ using key = std::pair <std::string, std::string>;
 //Creamos el unordered_map
 std::unordered_map <key, std::string, pair_hash> combinaciones_map;
 
+//Creamos la variable puntuacion
+int puntuacion;
+
 //Leer y guardar archivo
 void Combinaciones()
 {
@@ -42,6 +45,8 @@ void Combinaciones()
 	int espacio, mas, fin, longitud, inicio;
 	inicio = 0;
 	key key1;
+
+	//bucle para obtener el unordered_map
 	while (getline(fentrada, linea))
 	{
 		//find
@@ -49,54 +54,58 @@ void Combinaciones()
 		mas = linea.find("+");
 		fin = linea.find("\n");
 
-		longitud = espacio - inicio;
+		longitud = espacio - 1;
 		element = linea.substr(inicio, longitud);
-		inicio = espacio + 1;
-		longitud = mas - inicio;
+		inicio = espacio + 2;
+		longitud = (mas - 1) - (espacio + 2);
 		key1.first = linea.substr(inicio, longitud);
-		inicio = mas + 1;
+		inicio = mas + 2;
 		longitud = fin - inicio;
-		key1.second = linea.substr(inicio, longitud);
+		key1.second = linea.substr(inicio);
 		inicio = 0;
 
-		combinaciones_map[key1] = element;
-		//std::cout << element << "=" << key.first << "+" << key.second << std::endl;	
+		combinaciones_map[key1] = {element};
 	}
+
 	//cerrar archivo
 	fentrada.close();
 }
 std::vector <std::string> basics({"Air", "Earth", "Fire", "Water"});
 std::vector <std::string> elements (basics);
-//std::vector <std::string> elements({"Air", "Earth", "Fire", "Water"});
 
 //funcion para imprimir los nuevos elementos
 void Print()
 {
+	std::cout << "Your score is: " << puntuacion << std::endl;
+	std::cout << "You have these elements: " << std::endl;
 	for (int i = 0; i < elements.size(); i++)
 	{
 		std::cout << i + 1 << " " << elements[i] << std::endl;
 	}
+	
 }
 //Declaramos que existe la funcion delete
 void Delete(int pos);
-
+//void NewElement();
 //funcion combinar
 void Combinar(int pos1, int pos2)
 {
 	key aux;
 	if (pos1 != pos2)
 	{
-		/*if (combinaciones_map[aux] == NULL) 
-		{
-			aux.first = elements[pos2 ];
-			aux.second = elements[pos1];
-		}*/
 		aux.first = elements[pos1-1];
 		aux.second = elements[pos2-1];
-
-		elements.insert(elements.end(), combinaciones_map[aux]);
+		auto search = combinaciones_map.find({ aux.first, aux.second });
+		
+		if (!combinaciones_map.count(aux))
+		{
+			aux.first = elements[pos2 - 1];
+			aux.second = elements[pos1 - 1];
+			search = combinaciones_map.find({ aux.first, aux.second });		
+		}		
+		elements.insert(elements.end(), search->second);
+		Delete(pos1);
 		Delete(pos2);
-		Delete(pos1); 		
 	}
 	else
 	{
@@ -140,19 +149,8 @@ void Sort()
 
 //funcion clean
 void Clean()
-{/*
+{
 	Sort();
-	for (int i = 0; i < elements.size(); i++)
-	{
-		for (int j = elements.size() - 1; j > i; j--)
-		{
-			if (elements[i] == elements [j])
-			{
-				Delete(j);
-			}
-		}
-	}*/
-
 	elements.erase(unique(elements.begin(), elements.end()), elements.end());
 }
 void Help() 
@@ -160,7 +158,7 @@ void Help()
 		std::cout << "--------------------------------------------------------------" << std::endl;
 		std::cout << "                       P2_G35's ALCHEMY                       " << std::endl;
 		std::cout << "--------------------------------------------------------------" << std::endl;
-		std::cout << "- Enter two numbers of your elements list to combine them." << std::endl;
+		std::cout << "- Enter the word 'combine' and two numbers of your elements list to combine them." << std::endl;
 		std::cout << "- Enter the word 'add' and the number of an element to add a new instance of that element." << std::endl;
 		std::cout << "- Enter 'add basics' to add new instances of the 4 basic elements." << std::endl;
 		std::cout << "- Enter the word 'delete' and the number of an element to erase it from your list." << std::endl;
@@ -172,9 +170,26 @@ void Help()
 		
 }
 
+//funcion elemento nuevo
+void NewElement(std::string element)
+{
+	bool repetido = false;
+
+	for (int i = 0; i < elements.size() - 1; i++)
+	{
+		if (elements.end() == elements.begin() + i)
+		{
+			repetido = true;
+		}
+	}
+	if (!repetido)
+	{
+		puntuacion++;
+	}
+}
+
 void main()
 {
-	int puntuacion{ 0 };
 	std::string input;
 	Combinaciones();
 	Help();
@@ -200,15 +215,15 @@ void main()
 			{
 				Addbasics();
 			}
-			else if (aux == "%d" )
+			else /*if (aux == "%d" )*/
 			{				
 				int num;
 				num = stoi(aux);
 				Add(num);
 			}
-			else {
+			/*else {
 				std::cout << "Error" << std::endl;
-			}
+			}*/
 		}
 		else if (input == "info")
 		{
@@ -239,8 +254,9 @@ void main()
 			int aux1;
 			int aux2;
 			std::cin >> aux1 >> aux2;	
-			Combinar(aux1,aux2);			
+			Combinar(aux1,aux2);	
 		}
+
 		else 
 		{ 
 			std::cout << "Error" << std::endl;
--- Alchemy/SourcePruebasMeri.cpp
+++ Alchemy/SourcePruebasMeri.cpp
@@ -38,4 +38,46 @@ std::cin >> posicion;
 std::string url;
 url = "https://en.wikipedia.org/wiki/" + element[posicion]
 ShellExecuteA(nullptr, "open", url.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+}
+
+void Clean()
+{
+	Sort();
+	for (int i = 0; i < elements.size(); i++)
+	{
+	for (int j = elements.size() - 1; j > i; j--)
+	{
+	if (elements[i] == elements [j])
+	{
+	Delete(j);
+	}
+	}
+	}
+
+	elements.erase(unique(elements.begin(), elements.end()), elements.end());
+}
+void Combinar(int pos1, int pos2)
+{
+key aux;
+if (pos1 != pos2)
+{
+bool existe;
+aux.first = elements[pos1-1];
+aux.second = elements[pos2-1];
+
+auto search = combinaciones_map.find({ aux.first, aux.second });
+if (it)
+{
+
+}
+
+
+elements.insert(elements.end(), search -> second);
+Delete(pos2);
+Delete(pos1);
+}
+else
+{
+std::cout << "Error" << std::endl;
+}
 }*/
